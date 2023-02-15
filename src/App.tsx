9d57d9462c256bb05b01e90fc7af49ffe39ddb06
ramy100 <ramy.alashmawy@outlook.com>
2023-02-15 12:24:17 +0200

    adding colors and font and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="h-full">
+      Hello world
+      <div className="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      mobile: '375px',
+      desktop: '1366px',
+    },
+    extend: {
+      gradientColorStops: {
+        scissors: ['hsl(39, 89%, 49%)', 'hsl(40, 84%, 53%)'],
+        paper: ['hsl(230, 89%, 62%)', 'hsl(230, 89%, 65%)'],
+        rock: ['hsl(349, 71%, 52%)', 'hsl(349, 70%, 56%)'],
+        lizard: ['hsl(261, 73%, 60%)', 'hsl(261, 72%, 63%)'],
+        cyan: ['hsl(189, 59%, 53%)', 'hsl(189, 58%, 57%)'],
+      },
+      colors: {
+        dark: 'hsl(229, 25%, 31%)',
+        score: 'hsl(229, 64%, 46%)',
+        header: 'hsl(217, 16%, 45%)',
+      },
+    },
   },
   plugins: [],
 };
